9c8e6f29500647022f4eaef50a936035213750d0
gsxhnd <gsxhnd@gmail.com>
2019-12-29 12:32:56 +0800

    update

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/gsxhnd/Lang/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/gsxhnd/Code/BangumiApp"
-export "FLUTTER_TARGET=/Users/gsxhnd/Code/BangumiApp/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/gsxhnd/Code/bangumi"
+export "FLUTTER_TARGET=/Users/gsxhnd/Code/bangumi/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/gsxhnd/Lang/flutter/bin/cache/artifacts/engine/ios"
