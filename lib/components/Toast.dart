0de9576779b6382ea8451663db64a2a22c299371
gsxhnd <gsxhnd@gmail.com>
2020-08-25 15:36:49 +0800

    chore: update dep

--- lib/components/Toast.dart
+++ lib/components/Toast.dart
@@ -1,12 +1,13 @@
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/material.dart';
 
+
 class Toast {
   static void warm (String msg) {
     Fluttertoast.showToast(
         msg: msg,
         gravity: ToastGravity.BOTTOM,
-        timeInSecForIos: 1,
+        timeInSecForIosWeb: 1,
         backgroundColor: Color.fromARGB(190, 0, 0, 0),
         textColor: Colors.white,
         fontSize: 16.0);
@@ -16,7 +17,7 @@ class Toast {
     Fluttertoast.showToast(
         msg: msg,
         gravity: ToastGravity.BOTTOM,
-        timeInSecForIos: 1,
+        timeInSecForIosWeb: 1,
         backgroundColor: Color.fromARGB(190, 0, 0, 0),
         textColor: Colors.white,
         fontSize: 16.0);
@@ -26,7 +27,7 @@ class Toast {
     Fluttertoast.showToast(
         msg: msg,
         gravity: ToastGravity.BOTTOM,
-        timeInSecForIos: 1,
+        timeInSecForIosWeb: 1,
         backgroundColor: Color.fromARGB(190, 183, 50, 39),
         textColor: Colors.white,
         fontSize: 16.0);
